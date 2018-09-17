1f9ae32f2497f4d2cfbf591f2718495f97ea3242
Harunobu Agematsu <at.mc1.18@gmail.com>
2018-09-17 20:44:44 +0900

    chapter 2, 複数ページの移動

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse
 
 def index(request):
     params = {
-        'title' : 'Hello/Index',
-        'msg'   : 'これは、サンプルで作ってみたページでござる。'
+        'title': 'Hello/Index',
+        'msg': 'これは、サンプルで作ってみたページでござる。',
+        'goto': 'next'
+    }
+    return render(request, 'hello/index.html', params)
+
+
+def next(request):
+    params = {
+        'title': 'Hello/Next',
+        'msg': 'これは、もう１つのページでござる。',
+        'goto': 'index'
     }
     return render(request, 'hello/index.html', params)
