654490f80d3d0f2ac01dead2aa2a6b7bb49affb9
Harunobu Agematsu <at.mc1.18@gmail.com>
2018-09-20 22:21:30 +0900

    chapter 3-4, ModelFormを使う

--- hello/forms.py
+++ hello/forms.py
@@ -1,10 +1,8 @@
 from django import forms
+from .models import Friend
 
 
-class HelloForm(forms.Form):
-    name = forms.CharField(label='Name')
-    mail = forms.CharField(label='Mail')
-    gender = forms.BooleanField(label='Gender', required=False)
-    age = forms.IntegerField(label='Age')
-    birthday = forms.DateField(label='Birth')
-
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name', 'mail', 'gender', 'age', 'birthday']
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from .forms import FriendForm
 from .models import Friend
 from django.shortcuts import redirect
 
@@ -8,54 +7,24 @@ from django.shortcuts import redirect
 def index(request):
 
     data = Friend.objects.all()
-
     params = {
         'title': 'Hello',
         'data': data
     }
-
     return render(request, 'hello/index.html', params)
 
 
 def create(request):
 
-    params = {
-        'title': 'Hello',
-        'form': HelloForm(),
-    }
-
     if request.method == 'POST':
-        name = request.POST['name']
-        mail = request.POST['mail']
-        gender = 'gender' in request.POST
-        age = request.POST['age']
-        birth = request.POST['birthday']
-
-        friend = Friend(name=name, mail=mail, gender=gender, age=age, birthday=birth)
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
         friend.save()
         return redirect(to='/hello')
 
+    params = {
+        'title': 'Hello',
+        'form': FriendForm(),
+    }
     return render(request, 'hello/create.html', params)
 
-
-class HelloView(TemplateView):
-
-    def __init__(self):
-        self.params = {
-            'title': 'Hello',
-            'message': 'your data',
-            'form': HelloForm()
-        }
-
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
-
-    def post(self, request):
-        msg = 'あなたは、<b>' + request.POST['name'] + \
-            '(' + request.POST['age'] + ') </b> さんです' + \
-            '<br>メールアドレスは<b>' + request.POST['mail'] + '</b>でござろう。'
-
-        self.params['message'] = msg
-        self.params['form'] = HelloForm(request.POST)
-
-        return render(request, 'hello/index.html', self.params)
