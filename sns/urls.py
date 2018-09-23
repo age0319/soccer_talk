98515ad372a05fdfaa02b94e962a304a117ea6fa
Harunobu Agematsu <at.mc1.18@gmail.com>
2018-09-23 11:27:21 +0900

    chapter 5, 本格アプリ作りに挑戦

--- sns/urls.py
+++ sns/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('groups', views.groups, name='groups'),
+    path('add', views.add, name='add'),
+    path('creategroup', views.creategroup, name='creategroup'),
     path('post', views.post, name='post'),
-    path('find', views.find, name='find'),
     path('share/<int:share_id>', views.share, name='share'),
     path('good/<int:good_id>', views.good, name='good'),
 ]
+
--- soccer/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class FindForm(forms.Form):
-    find = forms.CharField(max_length=50)
-
-
-class MessageForm(forms.Form):
-    content = forms.CharField(max_length=500, widget=forms.Textarea)
